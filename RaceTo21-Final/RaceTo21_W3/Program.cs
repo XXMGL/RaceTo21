f38f1bdf277fd136083a54b24fc0e13fa97eda54
XXMGL <113867909+XXMGL@users.noreply.github.com>
2023-02-16 19:09:47 -0500

    Add files via upload

---
+++ RaceTo21-Final/RaceTo21_W3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RaceTo21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CardTable cardTable = new CardTable();
+            Game game = new Game(cardTable);
+            while (game.nextTask != Task.GameOver)//run the game.DoNextTask function until the task go to GameOver
+            {
+                game.DoNextTask();
+            }
+        }
+    }
+}
+
